96aaa8a3beb76168e08b635c3ddb4d801d4b6083
kezstain <kezstain@gmail.com>
2016-06-16 21:37:38 +0930

    Added contracts example

---
+++ ContractsExample/AfterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PostSharp.Patterns.Contracts;
+
+namespace ContractsExample
+{
+    [TestClass]
+    public class AfterTests
+    {
+        [TestMethod]
+        public void TestGetFullNameValid()
+        {
+            var firstName = "Joe";
+            var lastName = "Bloggs";
+            var result = GetFullName(firstName, lastName);
+            Assert.AreEqual(result, "Joe Bloggs");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetFullNameWithNullFirstName()
+        {
+            var lastName = "Bloggs";
+            GetFullName(null, lastName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetFullNameWithNullLastName()
+        {
+            var firstName = "Joe";
+            GetFullName(firstName, null);
+        }
+
+        public string GetFullName([NotNull] string firstName,[NotNull] string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+    }
+}
---
+++ ContractsExample/BeforeTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContractsExample
+{
+    [TestClass]
+    public class BeforeTests
+    {
+        [TestMethod]
+        public void TestGetFullNameValid()
+        {
+            var firstName = "Joe";
+            var lastName = "Bloggs";
+            var result = GetFullName(firstName, lastName);
+            Assert.AreEqual(result, "Joe Bloggs");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetFullNameWithNullFirstName()
+        {
+            var lastName = "Bloggs";
+            GetFullName(null, lastName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetFullNameWithNullLastName()
+        {
+            var firstName = "Joe";
+            GetFullName(firstName, null);
+        }
+
+        public string GetFullName(string firstName, string lastName)
+        {
+            if (firstName == null)
+                throw new ArgumentNullException();
+
+            if (lastName == null)
+                throw new ArgumentNullException();
+
+            return firstName + " " + lastName;
+        }
+    }
+}
